e8fb7a215ea50dfc2f8fda8dc49755201408109e
Ariana Espinoza <55963646+Ariana1998@users.noreply.github.com>
2021-10-21 18:46:24 -0800

    Initial commit

---
+++ app/src/main/java/espinoza/ariana/digimind/Recordatorio.kt
@@ -0,0 +1,7 @@
+package espinoza.ariana.digimind
+
+import java.io.Serializable
+
+class Recordatorio (var dias:String,
+                    var tiempo:String,
+                    var nombre:String):Serializable
---
+++ app/src/main/java/espinoza/ariana/digimind/Task.kt
@@ -0,0 +1,6 @@
+package espinoza.ariana.digimind
+
+class Task (var title:String,
+            var days:ArrayList<String>,
+            var time:String
+)
